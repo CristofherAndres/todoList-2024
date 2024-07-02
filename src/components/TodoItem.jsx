bdff9536ebab61db84f2f6001b79271c4abbca58
Cristofher <Cristofherrojas.cr@gmail.com>
2024-07-02 15:23:51 -0400

    Aveces clase 01 de julio

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,8 +3,9 @@ import {Fragment} from 'react';
 const TodoItem = (props) => {
 	return (
 		<Fragment>
-			<li className="list-group-item">
+			<li className="list-group-item d-flex justify-content-between">
 			{props.todo.tarea}
+			<input type='checkbox' className='form-checked-input me-2' checked={props.todo.estado}/>
 			</li>
 		</Fragment>
 	);
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,35 +1,61 @@
-import {Fragment, useState} from 'react';
+import {Fragment, useState, useRef} from 'react';
+
+/* Importamos id para las tareas, unico y aleatorio */
+
+import uuid4 from 'uuid4';
 
 import TodoItem from './TodoItem';
 
 const TodoList = () => {
-	const [todos, setTodos] = useState(
-		[
-			{id: 1, tarea: 'Estudiar React'},
-			{id: 2, tarea: 'Hacer la compra'},
-			{id: 3, tarea: 'Llamar a mamá'},
-			{id: 4, tarea: 'Sacar al perro'},
-			{id: 5, tarea: 'Hacer la comida'},
-			{id: 6, tarea: 'Hacer la cama'},
-			{id: 7, tarea: 'Lavar los platos'},
-			{id: 8, tarea: 'Hacer ejercicio'}
-		]
-	)
+	const [todos, setTodos] = useState([
+		{id: 1, tarea: 'Estudiar React', estado: false},
+		{id: 2, tarea: 'Hacer la compra', estado: true},
+		{id: 3, tarea: 'Llamar a mamá', estado: false},
+		{id: 4, tarea: 'Sacar al perro', estado: true},
+	]);
+
+	const agregarTarea = () => {
+		const tarea = tareaRef.current.value.trim();
+		if (tarea === '') return;
+		setTodos((prevTodos) => {
+			const nuevaTarea = {
+				id: uuid4(),
+				tarea: tarea,
+				estado: false,
+			};
+			return [...prevTodos, nuevaTarea];
+		});
+
+		tareaRef.current.value = null;
+	};
+
+	//Definir el tareaRef
+	const tareaRef = useRef();
+
 	return (
 		<Fragment>
-				<h1 className="display-5 my-3">Lista de tareas 😎✔️ </h1>
+			<h1 className="display-5 my-3">Lista de tareas 😎✔️ </h1>
 
-                <div className='input-group my-5'>
-                    <input type="text" className='form-control' placeholder='Ingrese una tarea' />
-                    <button className='btn btn-primary ms-2'><i className="bi bi-patch-plus"></i></button>
-                    <button className='btn btn-danger ms-2'><i className="bi bi-trash"></i></button>
-                </div>
+			<div className="input-group my-5">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Ingrese una tarea"
+					ref={tareaRef}
+				/>
+				<button className="btn btn-primary ms-2" onClick={agregarTarea}>
+					<i className="bi bi-patch-plus"></i>
+				</button>
+				<button className="btn btn-danger ms-2">
+					<i className="bi bi-trash"></i>
+				</button>
+			</div>
 
-				<ul className="list-group my-5">
+			<ul className="list-group my-5">
 				{todos.map((todo) => (
-						<TodoItem key={todo.id} todo={todo} />
-					))}
-				</ul>
+					<TodoItem key={todo.id} todo={todo} />
+				))}
+			</ul>
 		</Fragment>
 	);
 };
